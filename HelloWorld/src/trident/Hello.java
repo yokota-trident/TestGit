15a283ee0e6757782e20fa5cb48eff059f5672bd
tsit <tsit@j-tech>
2018-07-12 01:00:23 +0900

    Worldを１つ

--- HelloWorld/src/trident/Hello.java
+++ HelloWorld/src/trident/Hello.java
@@ -7,6 +7,8 @@ public class Hello {
 		System.out.println("Hello!!");
 		System.out.println("Hello!!");
 		System.out.println("Hello!!");
+		System.out.println("Hello!!");
+		System.out.println("World!!");
 	}
 
 }
